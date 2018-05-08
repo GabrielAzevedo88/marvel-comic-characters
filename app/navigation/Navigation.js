1687eaf9571e7445a75ad692cd1f0292adfa993c
Gabriel Azevedo <gabriel.azevedo.rs@gmail.com>
2018-05-07 21:36:26 -0300

    Creating caracters screen, commons functions and updat api

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { StyleSheet} from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import CharactersList from '../containers/CharactersList';
+import {  mapNavStateToProps } from '../functions/commons';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'red',
+  },
+});
 
 const AppNavigator = StackNavigator({
-
+  CharactersList: {
+    screen: CharactersList,
+    navigationOptions: {
+      title: 'Marvel comic characters',
+      headerTintColor: 'white',
+      headerStyle: styles.header,
+    },
+  },
 });
 
 export default AppNavigator;
